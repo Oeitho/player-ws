04e45ccd19dc560539bd54ea47b855f6e2dbf149
Øivind Thorrud <oeitho@gmail.com>
2020-10-06 14:28:36 +0200

    first commit

---
+++ src/test/java/com/NativeoeithoIT.java
@@ -0,0 +1,9 @@
+package com;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeoeithoIT extends oeithoTest {
+
+    // Execute the same tests but in native mode.
+}
